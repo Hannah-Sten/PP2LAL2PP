ee7612018dd062084fb3f9c617065f602a00498a
Ruben Schellekens <r.p.schellekens@gmail.com>
2016-04-26 23:05:24 +0200

    Compiler source init.

---
+++ compiler/src/nl/rubensten/pp2lal2pp/PP2LAL2PP.java
@@ -0,0 +1,15 @@
+package nl.rubensten.pp2lal2pp;
+
+/**
+ * Practicum Processor To Learn Assembly Language To Preserve Prosperity-Language compiler.
+ *
+ * @author Ruben Schellekens, Sten Wessel
+ * @version 0.1
+ */
+public class PP2LAL2PP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
