d3c3fa35f9be9408ddfd5dc1fb293155ac734c3b
Hannah Schellekens <r.p.schellekens@gmail.com>
2016-04-26 23:22:38 +0200

    Added Variable interface.

---
+++ compiler/src/nl/rubensten/pp2lal2pp/lang/Variable.java
@@ -0,0 +1,18 @@
+package nl.rubensten.pp2lal2pp.lang;
+
+/**
+ * @author Ruben Schellekens
+ */
+public interface Variable {
+
+    /**
+     * @return The memory address where the value of the variable is stored.
+     */
+    int getPointer();
+
+    /**
+     * @return The name of the variable.
+     */
+    String getName();
+
+}
