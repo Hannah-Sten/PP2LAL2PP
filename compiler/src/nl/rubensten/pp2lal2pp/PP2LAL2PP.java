95064453331885ecd68e85b0f656ba935bc75d97
Ruben Schellekens <r.p.schellekens@gmail.com>
2016-04-28 17:20:28 +0200

    Better tokeniser.

--- compiler/src/nl/rubensten/pp2lal2pp/PP2LAL2PP.java
+++ compiler/src/nl/rubensten/pp2lal2pp/PP2LAL2PP.java
@@ -1,5 +1,7 @@
 package nl.rubensten.pp2lal2pp;
 
+import nl.rubensten.pp2lal2pp.compiler.LineTokeniser;
+import nl.rubensten.pp2lal2pp.compiler.Tokeniser;
 import nl.rubensten.pp2lal2pp.parser.FileParser;
 
 import java.io.File;
@@ -15,6 +17,19 @@ public class PP2LAL2PP {
     public static void main(String[] args) {
         File file = new File("test.pp2");
         FileParser parser = new FileParser(file);
+
+        String code = "@#491(034!*****1:==14:=1:=>=123<41=213=<=12#><>5 bambi\n  3+x/4 is " +
+                "{kmaeel{}{{{}}{}{}{}{}{}{{}}}}}}  " +
+                " " +
+                "\n\t\t    de aller " +
+                "be\n  ste!";
+        LineTokeniser tokeniser = new LineTokeniser(code);
+        for (String line : tokeniser) {
+            Tokeniser tokeniser1 = new Tokeniser(line);
+            for (String token : tokeniser1) {
+                System.out.println(token);
+            }
+        }
     }
 
 }
