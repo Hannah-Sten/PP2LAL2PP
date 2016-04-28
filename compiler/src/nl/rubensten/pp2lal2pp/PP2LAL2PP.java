cb4e39c437bee6ecf0d794f456a367dcc7666a91
Ruben Schellekens <r.p.schellekens@gmail.com>
2016-04-28 16:28:56 +0200

    Added Tokenisers.

--- compiler/src/nl/rubensten/pp2lal2pp/PP2LAL2PP.java
+++ compiler/src/nl/rubensten/pp2lal2pp/PP2LAL2PP.java
@@ -1,5 +1,8 @@
 package nl.rubensten.pp2lal2pp;
 
+import nl.rubenlib.io.FileWorker;
+import nl.rubensten.pp2lal2pp.compiler.LineTokeniser;
+import nl.rubensten.pp2lal2pp.compiler.Tokeniser;
 import nl.rubensten.pp2lal2pp.parser.FileParser;
 
 import java.io.File;
---
+++ compiler/src/nl/rubensten/pp2lal2pp/compiler/LineTokeniser.java
@@ -0,0 +1,44 @@
+package nl.rubensten.pp2lal2pp.compiler;
+
+import com.sun.javafx.UnmodifiableArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Ruben Schellekens
+ */
+public class LineTokeniser implements Iterable<String> {
+
+    /**
+     * A list of all lines in the given code.
+     */
+    private List<String> lines;
+
+    public LineTokeniser(String code) {
+        String[] strings = code.split("(\\s)*\\n(\\s)*");
+        lines = new UnmodifiableArrayList<>(strings, strings.length);
+    }
+
+    /**
+     * Gets the line with line number <code>index</code> starting with 0.
+     */
+    public String getLine(int index) {
+        return lines.get(index);
+    }
+
+    /**
+     * @return The amount of lines in the tokeniser.
+     */
+    public int size() {
+        return lines.size();
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return lines.iterator();
+    }
+
+}
