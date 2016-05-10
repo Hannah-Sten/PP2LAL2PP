1ecb70ef01a44323a4ab2d2926209eaa4c51d5be
Hannah Schellekens <r.p.schellekens@gmail.com>
2016-05-10 18:55:37 +0200

    Added templates.

--- compiler/src/nl/rubensten/pp2lal2pp/compiler/Compiler.java
+++ compiler/src/nl/rubensten/pp2lal2pp/compiler/Compiler.java
@@ -2,10 +2,10 @@ package nl.rubensten.pp2lal2pp.compiler;
 
 import nl.rubensten.pp2lal2pp.CompilerException;
 import nl.rubensten.pp2lal2pp.lang.Program;
+import nl.rubensten.pp2lal2pp.util.FileWorker;
+import nl.rubensten.pp2lal2pp.util.Template;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 /**
  * @author Ruben Schellekens
@@ -22,23 +22,47 @@ public class Compiler {
      */
     private Program input;
 
+    /**
+     * The compiled output assembly code.
+     */
+    private StringBuilder assembly;
+
     public Compiler(File output, Program input) {
         this.output = output;
         this.input = input;
+        this.assembly = new StringBuilder();
     }
 
     /**
      * Compiles the input and writes it to the given output file.
      */
     public void compile() throws CompilerException {
-        try {
-            FileWriter fw = new FileWriter(output);
-            fw.write("\nTEST\n");
-            fw.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Standard header
+        assembly.append(Template.BEGIN_CODE);
+        assembly.append("\n\n");
+        assembly.append(Template.DEFAULT_EQU);
+        assembly.append("\n\n");
+        assembly.append(Template.BEGIN_MAIN);
+        assembly.append("\n\n");
+
+        // Main function
+
+
+        // Other functions
+
+
+        // @END
+        assembly.append(Template.END);
+
+        // Write to file
+        write();
+    }
+
+    /**
+     * Writes the compiled output to the output file.
+     */
+    private void write() {
+        new FileWorker(output).write(assembly.toString(), true);
     }
 
 }
---
+++ compiler/src/nl/rubensten/pp2lal2pp/util/Template.java
@@ -0,0 +1,64 @@
+package nl.rubensten.pp2lal2pp.util;
+
+/**
+ * @author Ruben Schellekens
+ */
+public enum Template {
+
+    /**
+     * Denotes the start of the program.
+     */
+    BEGIN_CODE("begin-code.template"),
+
+    /**
+     * The code to jump to the main function.
+     */
+    BEGIN_MAIN("begin-main.template"),
+
+    /**
+     * The default used EQU statements at the beginning of the program.
+     */
+    DEFAULT_EQU("default-equ.template"),
+
+    /**
+     * Denotes the end of the program.
+     */
+    END("end.template");
+
+    /**
+     * The directory where the templates are stored followed by a slash.
+     */
+    private static String DIR = "/template/";
+
+    /**
+     * The file path relative to the program's root and {@link Template#DIR} where the template is
+     * located.
+     */
+    private String filePath;
+
+    Template(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Loads the contents of the template.
+     *
+     * @return The contents of the template.
+     */
+    public String load() {
+        StreamWorker sw = new StreamWorker(getClass().getResourceAsStream(getPath()));
+        return sw.read();
+    }
+
+    public String getPath() {
+        return Template.DIR + filePath;
+    }
+
+    @Override
+    /**
+     * Calls {@link Template#load()}
+     */
+    public String toString() {
+        return load();
+    }
+}
